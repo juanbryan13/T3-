b9649384fc4a889fdb6473b080c1f6ef172d460e
Juan Bryan <N00495703@upn.pe>
2026-06-15 13:00:03 -0500

    Limpieza de basura

--- obj/Debug/net10.0/te tres.AssemblyInfo.cs
+++ obj/Debug/net10.0/te tres.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("te tres")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c5c2970fbcbbfcac9c371783febbd39d5d5783")]
 [assembly: System.Reflection.AssemblyProductAttribute("te tres")]
 [assembly: System.Reflection.AssemblyTitleAttribute("te tres")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
